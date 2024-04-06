22c46dc975d3992035f99b06e0be5e6399168071
oneneoncoffee <76235757+oneneoncoffee@users.noreply.github.com>
2024-04-05 22:24:10 -0600

    Create luxmus.c
    Patch or Rewrite of mus.c - Ubuntu adaptation of the chromebook version of source code.. please note mus.c will not work outside of the cromebook simulation in part due to, cross-platform library issues. Maybe this is googles hack job of defanging the beast we all love GCC. Chrome OS, the operating system on Chromebooks, is based on Linux but has certain restrictions that limit access to system-level functionalities. This can make it challenging to install and use development tools like GCC, especially if you're looking to perform tasks that require low-level system access.
    
    Chrome OS emphasizes simplicity and security, which means users may need to navigate additional layers of abstraction or use tools like Linux containers to access the full suite of development tools available on Linux systems. Is it security or something other  then just don't be evil ? While it's certainly possible to use GCC on a Chromebook with some effort and potentially additional tools like Crostini (Linux container support on Chrome OS), the experience may not be as um, leak free or seamless as on a traditional Linux development environment.
    
    After all free as-in freedom!

---
+++ luxmus.c
@@ -0,0 +1,123 @@
+// Rewrite - Ubuntu adaptation of the chromebook version of source code..
+// please note mus.c will not work outside of the cromebook simulation in part due to,
+// cross-platform library issues. Maybe this is googles hack job of defanging the beast 
+// we all love gcc. Chrome OS, the operating system on Chromebooks, is based on Linux 
+// but has certain restrictions that limit access to system-level functionalities. 
+// This can make it challenging to install and use development tools like GCC, especially 
+// if you're looking to perform tasks that require low-level system access.
+// 
+// Chrome OS emphasizes simplicity and security, which means users may need to navigate 
+// additional layers of abstraction or use tools like Linux containers to access the full 
+// suite of development tools available on Linux systems. Is it security or something other  
+// then just don't be evil ? While it's certainly possible to use GCC on a Chromebook with 
+// some effort and potentially additional tools like Crostini (Linux container support on Chrome OS), t
+// he experience may not be as um, leak free or seamless as on a traditional Linux development environment.
+// After all free as-in freedom! 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/statvfs.h>
+#include <unistd.h>
+#include <time.h> // Added for timespec structure
+
+#define RED     "\x1B[31m"
+#define GREEN   "\x1B[32m"
+#define YELLOW  "\x1B[33m"
+#define RESET   "\x1B[0m"
+#define program_name "mus"
+#define program_version "3.6c"
+
+void print_usage();
+
+void print_usage() {
+    printf("Usage: %s <path_to_directory> \n", program_name);
+    printf("Examples:\n%s .. \n%s /\n", program_name, program_name);
+    printf("Program: %s Version %s\n", program_name, program_version);
+    printf("Magic Utility Storage analytics for Linux.\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        print_usage();
+        return 1;
+    }
+
+    struct statvfs vfs;
+    const char *path_argv = argv[1];
+    struct statvfs stat;
+    if (statvfs("/", &vfs) != 0) {
+        perror("statvfs");
+        return 1;
+    }
+    if (statvfs(path_argv, &stat) != 0) {
+        perror("Error while getting file system statistics");
+        return 1;
+    }
+
+    unsigned long long block_size = stat.f_frsize;
+    unsigned long long total_blocks = stat.f_blocks;
+    unsigned long long free_blocks = stat.f_bfree;
+    double used_percent = ((total_blocks - free_blocks) * 100.0) / total_blocks;
+    double free_percent = (free_blocks * 100.0) / total_blocks;
+    printf("Disk Usage for %s:\n", path_argv);
+    printf("Total Space.: %.2f MB\n", (total_blocks * block_size) / (1024.0 * 1024));
+    printf("Used Space..: %.2f MB (%.2f%%)\n", ((total_blocks - free_blocks) * block_size) / (1024.0 * 1024), used_percent);
+    printf("Free Space..: %.2f MB (%.2f%%)\n", (free_blocks * block_size) / (1024.0 * 1024), free_percent);
+    if (used_percent >= 90.0) {
+        printf(RED "Warning: Disk usage is high!\n" RESET);
+    } else if (used_percent >= 80.0) {
+        printf(YELLOW "Warning: Disk usage is moderate.\n" RESET);
+    } else {
+        printf(GREEN "Disk usage is normal.\n" RESET);
+    }
+
+    unsigned long long total_space = (unsigned long long) vfs.f_blocks * vfs.f_frsize;
+    unsigned long long free_space = (unsigned long long) vfs.f_bfree * vfs.f_frsize;
+    unsigned long long used_space = total_space - free_space;
+    double percent_free = (double) free_space / total_space * 100.0;
+    double percent_used = (double) used_space / total_space * 100.0;
+
+    printf("Total space.: %llu bytes\n", total_space);
+    printf("Used space..: %llu bytes (%.2lf%%)\n", used_space, percent_used);
+    printf("Free space..: %llu bytes (%.2lf%%)\n", free_space, percent_free);
+    printf("Total Space.: %.2f GB (%.2f TB)\n", (total_blocks * block_size) / (1024.0 * 1024 * 1024), (total_blocks * block_size) / (1024.0 * 1024 * 1024 * 1024));
+    printf("Used Space..: %.2f GB (%.2f%%)\n", ((total_blocks - free_blocks) * block_size) / (1024.0 * 1024 * 1024), used_percent);
+    printf("Free Space..: %.2f GB (%.2f%%)\n", (free_blocks * block_size) / (1024.0 * 1024 * 1024), free_percent);
+
+    unsigned int delayMillis = 1000;
+    struct timespec delayTime = {0, delayMillis * 1000000L}; // timespec structure for nanosleep
+    char choice;
+
+    while (1) {
+        printf("This will loop 1600 times and then display this exit prompt,\n");
+        printf("Do you want to continue (y/n)? ");
+        scanf(" %c", &choice);
+
+        if (choice == 'y' || choice == 'Y') {
+            printf("Continuing...\n");
+        } else if (choice == 'n' || choice == 'N') {
+            break;
+        } else {
+            printf("Invalid input. Please enter 'y' or 'n'.\n");
+            continue;
+        }
+
+        for (int x = 0; x < 1600; x++) {
+            // Redo disk usage calculations
+            used_percent = ((total_blocks - free_blocks) * 100.0) / total_blocks;
+            free_percent = (free_blocks * 100.0) / total_blocks;
+
+            // Print disk usage info
+            printf("Disk Usage for %s:\n", path_argv);
+            printf("Total Space.: %.2f MB\n", (total_blocks * block_size) / (1024.0 * 1024));
+            printf("Used Space..: %.2f MB (%.2f%%)\n", ((total_blocks - free_blocks) * block_size) / (1024.0 * 1024), used_percent);
+            printf("Free Space..: %.2f MB (%.2f%%)\n", (free_blocks * block_size) / (1024.0 * 1024), free_percent);
+
+            // Print loop count
+            printf("\nTotal loop count out of 1600 is %d....\n", x);
+            nanosleep(&delayTime, NULL); // Delay using nanosleep
+            system("clear"); // or system("cls") for Windows
+        }
+    }
+    return 0;
+}
